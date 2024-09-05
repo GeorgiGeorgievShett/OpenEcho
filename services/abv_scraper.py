39c1d4ecafc19e5427655cd6047d5179b4265343
Georgi Georgiev <ggeorgiev94@yahoo.co.uk>
2024-09-05 15:19:37 +0100

    Changes: Added services folder for any scraping service files and wrote a script for ABV.
    Required future steps:
    Clear popular_sites.txt from sites that do not have a register/forgotten password page or a method to check if an account is registered or not.
    
    Add a big file for printing out results and start writing scrapers around it. Discuss how this can be approached best. (GUI/Terminal?)

---
+++ services/abv_scraper.py
@@ -0,0 +1,42 @@
+import requests
+
+def check_username_registration(username):
+    base_url = 'https://passport.abv.bg/app/profiles/validateename'
+
+    params = {
+        'id' : username,
+        'fname': '',
+        'lname': '',
+        'byear': ''
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status() # Check for HTTP errors
+
+        response_text = response.text
+
+        if '<result>info.showcaptcha</result>' in response_text:
+            print("Captcha required. Unable to proceed.")
+            return None
+
+        if '<result>info.occupied</result>' in response_text:
+            return True
+        elif '<result>info.free</result>' in response_text:
+            return False
+        else:
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured: {e}")
+        return None
+
+username_to_check = 'testuser' # Replace with the actual username you want to check
+is_registered = check_username_registration(username_to_check)
+
+if is_registered is None:
+    print("Could not determine the registration status.")
+elif is_registered:
+    print(f"The username {username_to_check} is already registered.")
+else:
+    print(f"The username {username_to_check} is not registered.")
