7f3eee78002f53bbb12d531923c844c3d653e91b
Georgi Georgiev <ggeorgiev94@yahoo.co.uk>
2024-12-27 13:09:23 +0000

    fixed forlife, needs testing

--- services/forlife.py
+++ services/forlife.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 def check_forlife_email(email):
     session = requests.Session()
@@ -10,10 +11,20 @@ def check_forlife_email(email):
     except requests.RequestException:
         return "request_error"
 
+    try:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        nonce_input = soup.find("input", {"name": "woocommerce-login-nonce"})
+        if nonce_input and nonce_input["value"]:
+            login_nonce = nonce_input["value"]
+        else:
+            return "request_error"
+    except Exception:
+        return "request_error"
+
     payload = {
         "username": email,
         "password": "321321321",
-        "woocommerce-login-nonce": "fde8982a0c",
+        "woocommerce-login-nonce": login_nonce,
         "_wp_http_referer": "/my-account",
         "login": "Влизане"
     }
@@ -22,7 +33,7 @@ def check_forlife_email(email):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
         "Content-Type": "application/x-www-form-urlencoded"
     }
-    
+
     try:
         login_url = "https://www.forlife.bg/my-account"
         response = session.post(login_url, headers=headers, data=payload)
