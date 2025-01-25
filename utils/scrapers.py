c366d87b783e5a9bb1fe53071e1dedb64e417521
Georgi Georgiev <ggeorgiev94@yahoo.co.uk>
2025-01-25 15:16:03 +0000

    Added pragmaticbg and essentiallybg.

--- utils/scrapers.py
+++ utils/scrapers.py
@@ -1,19 +1,20 @@
 from services import plovdiv24, mailbg, techoffnews, econt, abv_scraper, sportal
 from services import arenabg, pomagalo, teenproblem, dnevnik, burgas24
 from services import varna24, mediapool, chitanka, forlife, vitamag
-from services import bgeprosveta, twinkl, cyberclubbg, bittel
+from services import bgeprosveta, twinkl, cyberclubbg, bittel, pragmaticbg
+from services import essentiallybg
 
 SCRAPERS = {
     "ABV.bg": abv_scraper.check_username_registration,
-    "e-prosveta": bgeprosveta.check_username_registration,
+    "Bg.e-prosveta.bg": bgeprosveta.check_username_registration,
     #"twinkl.bg": twinkl.check_username_registration,
-    "bittel.bg": bittel.check_username_registration,
-    "cyberclub.bg": cyberclubbg.check_username_registration,
+    "Bittel.bg": bittel.check_username_registration,
+    "Cyberclub.bg": cyberclubbg.check_username_registration,
     "Plovdiv24.bg": plovdiv24.check_username_registration,
-    "Mail.bg": mailbg.check_username_registration,
     "Offmedia.bg": techoffnews.check_username_registration,
     "Econt.com": econt.check_username_registration,
     "Sportal.bg": sportal.check_username_registration,
+    "Pragmatic.bg": pragmaticbg.check_username_registration,
     "Arenabg.com": arenabg.check_username_registration,
     "Pomagalo.com": pomagalo.check_username_registration,
     "Teenproblem.net": teenproblem.check_username_registration,
@@ -21,7 +22,9 @@ SCRAPERS = {
     "Burgas24.bg": burgas24.check_username_registration,
     "Varna24.bg": varna24.check_username_registration,
     "Mediapool.bg": mediapool.check_username_registration,
-    "Chitanka.info": chitanka.check_username_registration,
     "Vitamag.bg": vitamag.check_username_registration,
+    "Essentially.bg": essentiallybg.check_username_registration,
     "Forlife.bg": forlife.check_username_registration,
+    "Mail.bg": mailbg.check_username_registration,
+    "Chitanka.info": chitanka.check_username_registration,
 }
