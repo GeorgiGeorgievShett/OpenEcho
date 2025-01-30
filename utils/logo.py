8dab3a37c39d66f6c9343eec25b040185432065b
Georgi Georgiev <ggeorgiev94@yahoo.co.uk>
2025-01-30 12:04:42 +0000

    Added manual list

--- utils/logo.py
+++ utils/logo.py
@@ -9,5 +9,20 @@ def display_logo():
 
     """
     print(logo)
-    print("Welcome to OpenEcho - Email Registration Checker for popular Bulgarian websites.")
+    print("Welcome to OpenEcho - Email Registration Checker for popular and unpopular Bulgarian websites.")
     print("-------------------------------------------------\n")
+    print("Here is a list of sites which are worth checking manually:")
+    print("Here is a list of sites which are worth checking manually:")
+    print("""
+    - https://bg.airbnb.com/
+    - https://www.lcwaikiki.bg/bg-BG/BG/login
+    - https://banker.bg/wp-login.php
+    - https://mu-sofia.bg/wp-login.php
+    - https://tu-sofia.bg/login
+    - https://shop.gaming.bg/ // Въвели сте грешен имейл или парола // Не съществува клиент с такъв имейл //
+    - https://olx.bg/
+    - https://bg-mamma.com/
+    - https://onlinemashini.bg/
+    - https://kaufland.bg/
+    - https://auth.emag.bg/user/login
+    """)
